b7cda9b9ef2048eb67e0512e0051406feaffb0f1
darien rosero <darien.rosero2003@gmail.com>
2024-05-03 08:44:53 -0500

    {caracteristica}: se añadio la documentacion con swagger

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import { PORT } from './config/config.js'
 import adminRouts from './routs/adimin.routs.js'
 import usuariosRouts from './routs/usuarios.routs.js'
+import { swaggerDocs } from '../swaggerUi.js'
 
 const app = express()
 
@@ -11,4 +12,5 @@ app.use(adminRouts)
 
 app.use(usuariosRouts)
 
+swaggerDocs(app)
 app.listen(PORT, () => console.log(`server running on http://localhost:${PORT}`))
